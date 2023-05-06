e73fb86055df7b7a185dfe6afd9ede0c20e22f60
Jasmine <fancheny@cn.ibm.com>
2023-05-06 22:02:22 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="imooc-cli-test">vue3 template</div>
+</template>
+
+<script>
+export default {
+  name: 'ImoocCliTest',
+}
+</script>
+
+<style>
+#imooc-cli-test {
+  width: 100%;
+  height: 100%;
+}
+</style>
